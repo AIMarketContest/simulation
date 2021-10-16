b64697de7c853c669e0a473daa0bacd74b159311
Jack Latop <jack.benham34@gmail.com>
2021-10-16 15:27:34 +0100

    Add basic agent class shell

---
+++ src/agent.py
@@ -0,0 +1,7 @@
+class Agent:
+    pass
+
+    # lst_prices : a list of dictionaries - each element in the list is a time_step, the dictionary maps from an agent to a float (price)
+    # prior_sales_for_agent : int for prior sales
+    def get_price(self, lst_prices, prior_sales_for_agent):
+        pass
