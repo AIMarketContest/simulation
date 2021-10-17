826cbf59cd9e521eb083ce1bb01da57c09b8911f
Jack Benham <jack.benham34@gmail.com>
2021-10-17 02:06:27 +0100

    Reformatted Environment to comply with agreed style

--- src/agent.py
+++ src/agent.py
@@ -3,5 +3,5 @@ class Agent:
 
     # lst_prices : a list of dictionaries - each element in the list is a time_step, the dictionary maps from an agent to a float (price)
     # prior_sales_for_agent : int for prior sales
-    def get_price(self, lst_prices, prior_sales_for_agent):
+    def get_price(self, historic_set_prices: list[dict[Agent, float]], prior_sales_for_agent: int) -> float:
         pass
