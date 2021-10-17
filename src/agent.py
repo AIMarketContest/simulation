0707a2989b670bd4ffb9616310d6a76b90383e96
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-10-17 19:28:44 +0100

    Add subclass hook to Agent class

--- src/agent.py
+++ src/agent.py
@@ -10,19 +10,69 @@ class Agent(metaclass=ABCMeta):
     and given its sales for the last round
     and all prices set by competitors in that round
     it must give its new price.
-
-    Methods
-    -------
-    get_initial_price()
-        Represents the initial price of the product set by the agent (at timestep = 0).
-    get_price(last_round_all_agents_prices, last_round_sales)
-        Calculate the agent's price at the current timestep using data from the previous timestep.
     """
 
     @abstractmethod
-    def get_initial_price(self):
+    def get_initial_price(self) -> float:
+        """
+        Represents the initial price of the product set by the agent (at timestep = 0).
+
+        Returns
+        -------
+        float
+            Price of the product set by the agent at timestep = 0, discretised within [0,1].
+
+        Raises
+        ______
+        NotImplementedError
+            If concrete class does not override method.
+        """
+
         raise NotImplementedError
 
     @abstractmethod
-    def get_price(self, last_round_all_agents_prices, last_round_sales):
+    def get_price(
+        self,
+        last_round_all_agents_prices: list[float],
+        last_round_sales: int,
+        identity_index: int,
+    ) -> float:
+        """
+        Calculate the agent's price at the current timestep using data from the previous timestep.
+
+        Parameters
+        __________
+        last_round_all_agents_prices : list of float
+            List of all the prices set by all agents in the previous timestep.
+        last_round_sales: int
+            A positive integer representing the number of sales the agent made in the previous timestep.
+        identity_index: int
+            A positive integer that tells the agent which index in the list
+            corresponds to themself.
+
+        Returns
+        -------
+        float
+            Price of the product set by the agent at the current timestep, discretised within [0,1].
+
+        Raises
+        ______
+        NotImplementedError
+            If concrete class does not override method.
+        """
+
         raise NotImplementedError
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (
+            all(
+                [
+                    hasattr(subclass, "get_initial_price"),
+                    callable(subclass.get_initial_price),
+                    hasattr(subclass, "get_price"),
+                    callable(subclass.get_price),
+                ]
+            )
+            or NotImplemented
+        )
