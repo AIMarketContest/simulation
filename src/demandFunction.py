e2171dbb7d6445e4af114670e3e8930831a56e2a
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-10-18 20:21:56 +0100

    Add docstring for get_sales in DemandFunction

--- src/demandFunction.py
+++ src/demandFunction.py
@@ -1,9 +1,29 @@
 class DemandFunction:
     """
     The demand function is a map of the price of a product to its slaes.
-    
-    The demand function must give the quantity of the product 
+
+    The demand function must give the quantity of the product
     that will be sold by any agent in the market at a given price
     """
-    def get_sales(price : float) -> int:
+
+    def get_sales(price: float) -> int:
+        """
+        Calculates the quantity of the product that will be sold at a given price
+
+        Parameters
+        __________
+        price : float
+            The current price of the product set by some agent
+
+        Returns
+        _______
+        int
+            The quantity of the product that will be sold at the given price
+
+        Raises
+        ______
+        NotImplementedError
+            If concrete class does not override method.
+        """
+
         raise NotImplementedError
