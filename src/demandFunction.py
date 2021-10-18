034cc8987965cf2af892139ee949babbacd17d1c
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-10-18 20:16:42 +0100

    Add docstring for DemandFunction class

--- src/demandFunction.py
+++ src/demandFunction.py
@@ -1,4 +1,9 @@
 class DemandFunction:
+    """
+    The demand function is a map of the price of a product to its slaes.
     
-    def get_sales(all_agents_prices : list[float]) -> int:
+    The demand function must give the quantity of the product 
+    that will be sold by any agent in the market at a given price
+    """
+    def get_sales(price : float) -> int:
         raise NotImplementedError
