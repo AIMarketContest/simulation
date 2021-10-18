61a0229fa85d60700ec62d62db0548da7ed1d834
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-10-18 20:24:25 +0100

    Make get_sales an abstract method

--- src/demandFunction.py
+++ src/demandFunction.py
@@ -1,11 +1,15 @@
-class DemandFunction:
+from abc import ABCMeta, abstractmethod
+
+
+class DemandFunction(metaclass=ABCMeta):
     """
-    The demand function is a map of the price of a product to its slaes.
+    The demand function is a map of the price of a product to its sales.
 
     The demand function must give the quantity of the product
     that will be sold by any agent in the market at a given price
     """
 
+    @abstractmethod
     def get_sales(price: float) -> int:
         """
         Calculates the quantity of the product that will be sold at a given price
