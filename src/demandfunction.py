32703b910709e595c86c1f71abfa1b35db170d5d
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2021-10-21 03:48:54 +0100

    Add dummy implementation to demand function

--- src/demandfunction.py
+++ src/demandfunction.py
@@ -4,5 +4,8 @@ from agent import Agent
 class DemandFunction:
     pass
 
-    def get_sales(self, set_prices: dict[Agent, float]) -> dict[Agent, int]:
-        pass
+    def get_sales(self, set_prices: list[float]) -> list[int]:
+        demand_list = []
+        for _ in range(len(set_prices)):
+            demand_list.append(1)
+        return demand_list
