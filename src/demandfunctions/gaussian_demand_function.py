cf9b685f52879451b2307c24055c7e513436051f
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-10-22 13:53:04 +0100

    Use statistics library for normal dist

--- src/demandfunctions/gaussian_demand_function.py
+++ src/demandfunctions/gaussian_demand_function.py
@@ -1,5 +1,5 @@
 from demand_function import DemandFunction
-from scipy.stats import norm
+from statistics import NormalDist
 
 
 class GaussianDemandFunction(DemandFunction):
@@ -18,7 +18,7 @@ class GaussianDemandFunction(DemandFunction):
         """
         Parameters
         ----------
-        max_scale_factor: int, default=1
+        max_scale_factor: int, default=1000
             A positive integer representing the quantity to be sold
             from an agent if there was no price competiton
             i.e. all agents set the same price
@@ -36,7 +36,7 @@ class GaussianDemandFunction(DemandFunction):
     def get_sales(self, current_prices: list[float]) -> list[int]:
 
         N: int = len(current_prices)
-        gaussian_distribution: "norm" = norm(0.5, 1)
+        gaussian_distribution: "NormalDist" = NormalDist(mu=0.5, sigma=1)
         return [
             int((1 - gaussian_distribution.cdf(price)) * self.max_sales_scale_factor)
             for price in current_prices
