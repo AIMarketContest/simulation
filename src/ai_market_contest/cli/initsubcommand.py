464a16d9082ebf19bc8518b064513661c6dce738
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-11-01 23:47:59 +0000

    Make cli change name of agent file to user input

--- src/ai_market_contest/cli/initsubcommand.py
+++ src/ai_market_contest/cli/initsubcommand.py
@@ -1,13 +1,33 @@
 import os
 import pathlib
 import shutil
+import sys
+
+
+def make_agent_class(proj_dir, agent_name):
+    agent_filename = agent_name + ".py"
+    agent_file = proj_dir / agent_filename
+    agent_file.touch()
+    with agent_file.open("a") as f1:
+        with open("../agent.py", "r") as f2:
+            f1.writelines(f2.readlines())
 
 
 def initialise_file_structure(path):
+    proj_dir = path / "aicontest"
     if not path.is_dir():
         raise IllegalArgumentError
-    os.mkdir(path / "aicontest")
-    shutil.copyfile("../agent.py", path / "aicontest" / "agent.py")
+    if proj_dir.is_dir():
+        print(
+            "Agent already initialised\nIf you want to delete the current agent and start a new one,\nedit the agent.ini file\nthen run the command ai-market-contest restart <path>"
+        )
+        sys.exit(2)
+    print("Enter name of the agent ", end="")
+    agent_name = input()
+    print("Enter name(s) of the author(s): ", end="")
+    authors = input().split(",")
+    os.mkdir(proj_dir)
+    make_agent_class(proj_dir, agent_name)
 
 
 def create_subparser(subparsers):
