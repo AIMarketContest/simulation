a1fbddb05f8f5ff308a1d0abb846022c4221751a
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-11-03 10:10:55 +0000

    Make initsubcommand output a concrete agent class

--- src/ai_market_contest/cli/initsubcommand.py
+++ src/ai_market_contest/cli/initsubcommand.py
@@ -2,31 +2,66 @@ import os
 import pathlib
 import shutil
 import sys
+import atexit
 
 
-def make_agent_class(proj_dir, agent_name):
-    agent_filename = agent_name + ".py"
-    agent_file = proj_dir / agent_filename
+def make_agent_classname_camelcase(agent_name):
+    AGENT_STR = "agent"
+    agent_name_cc = agent_name.lower()
+    if AGENT_STR in agent_name_cc:
+        agent_name_cc = agent_name_cc.replace(AGENT_STR, AGENT_STR.capitalize())
+    return agent_name_cc[0].upper() + agent_name_cc[1:]
+
+
+def make_agent_class(proj_dir: pathlib.Path, agent_name: str):
+    IMPORT_STR = "import"
+    AGENT_STR = "Agent"
+    ABS_METHOD_STR = "abstractmethod"
+    CLASS_METHOD_STR = "classmethod"
+    agent_filename: str = agent_name + ".py"
+    agent_file: pathlib.Path = proj_dir / agent_filename
     agent_file.touch()
-    with agent_file.open("a") as f1:
+    with agent_file.open("w") as f1:
+        f1.write("from agent import Agent\n")
         with open("../agent.py", "r") as f2:
-            f1.writelines(f2.readlines())
+            for line in f2:
+                if line != None:
+                    if CLASS_METHOD_STR in line:
+                        break
+                    if IMPORT_STR in line:
+                        continue
+                    if ABS_METHOD_STR in line:
+                        continue
+                    if AGENT_STR in line:
+                        f1.write(
+                            "class "
+                            + make_agent_classname_camelcase(agent_name)
+                            + "(Agent):\n"
+                        )
+                    else:
+                        f1.write(line)
+        f2.close()
+    f1.close()
 
 
-def initialise_file_structure(path):
-    proj_dir = path / "aicontest"
-    if not path.is_dir():
+def make_proj_dir(path_exists, proj_dir):
+    if not path_exists:
         raise IllegalArgumentError
     if proj_dir.is_dir():
         print(
             "Agent already initialised\nIf you want to delete the current agent and start a new one,\nedit the agent.ini file\nthen run the command ai-market-contest restart <path>"
         )
         sys.exit(2)
+    os.mkdir(proj_dir)
+
+
+def initialise_file_structure(path):
+    proj_dir = path / "aicontest"
+    make_proj_dir(path.is_dir(), proj_dir)
     print("Enter name of the agent ", end="")
     agent_name = input()
     print("Enter name(s) of the author(s): ", end="")
     authors = input().split(",")
-    os.mkdir(proj_dir)
     make_agent_class(proj_dir, agent_name)
 
 
