1acc1875ff596fe3d5407423a1690132f0aa38f2
Michael Clasby <michaelclasby@hotmail.com>
2021-11-11 21:40:42 +0000

    Change env to use np arrays for major data (#17)
    * Change env to use np arrays for major data
    
    * Rework np types
    
    * Add test to check RuntimeError thrown when adding too many agents
    
    Co-authored-by: Pranav Maganti <pranav.maganti@gmail.com>

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,5 +1,7 @@
 ﻿from unittest import TestCase, mock
 
+import numpy as np
+
 from agents.fixed_agent import FixedAgent
 from demandfunctions.fixed_demand_function import FixedDemandFunction
 from environment import Environment
@@ -10,13 +12,13 @@ class EnvironmentTest(TestCase):
     simulation_length = 5
 
     def test_to_add_agent(self):
-        env = Environment(self.simulation_length, self.demand_function)
+        env = Environment(self.simulation_length, self.demand_function, 1)
         a = FixedAgent()
         env.add_agent(a)
         assert len(env.all_agents) == 1
 
     def test_to_add_agents(self):
-        env = Environment(self.simulation_length, self.demand_function)
+        env = Environment(self.simulation_length, self.demand_function, 4)
         a = FixedAgent()
         b = FixedAgent()
         c = FixedAgent()
@@ -29,11 +31,11 @@ class EnvironmentTest(TestCase):
         assert len(env.all_agents) == 4
 
     def test_to_get_results(self):
-        env = Environment(self.simulation_length, self.demand_function)
+        env = Environment(self.simulation_length, self.demand_function, 1)
         a = FixedAgent()
         env.add_agent(a)
-        test_set_prices = [[1.00]]
-        test_sales = [[4]]
+        test_set_prices = np.array([[1.00]])
+        test_sales = np.array([[4]])
         env.hist_set_prices = test_set_prices
         env.hist_sales_made = test_sales
 
@@ -41,15 +43,21 @@ class EnvironmentTest(TestCase):
         assert prices == test_set_prices and sales == test_sales
 
     def test_check_if_runs_simulation_for_correct_duration(self):
-        env = Environment(self.simulation_length, self.demand_function)
+        env = Environment(self.simulation_length, self.demand_function, 0)
         with self.assertRaises(IndexError):
             for _ in range(6):
                 env.run_next_time_step()
 
     def test_check_if_gets_price_called_on_time_step(self):
-        env = Environment(self.simulation_length, self.demand_function)
+        env = Environment(self.simulation_length, self.demand_function, 2)
         test_agent_a = mock.Mock(spec=FixedAgent)
         test_agent_b = mock.Mock(spec=FixedAgent)
+        test_agent_a.configure_mock(
+            **{"get_initial_price.return_value": 2.0, "get_price.return_value": 3.3}
+        )
+        test_agent_b.configure_mock(
+            **{"get_initial_price.return_value": 2.5, "get_price.return_value": 1.3}
+        )
 
         env.add_agent(test_agent_a)
         env.add_agent(test_agent_b)
@@ -59,3 +67,11 @@ class EnvironmentTest(TestCase):
         env.run_next_time_step()
         test_agent_a.get_price.assert_called_once()
         test_agent_b.get_price.assert_called_once()
+
+    def test_runtime_exception_when_too_many_agents_added(self):
+        env = Environment(self.simulation_length, self.demand_function, 2)
+        env.add_agent(FixedAgent())
+        env.add_agent(FixedAgent())
+
+        with self.assertRaises(RuntimeError):
+            env.add_agent(FixedAgent())
