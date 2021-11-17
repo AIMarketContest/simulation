105daab1d26667b805b882a1e9684bc839e72657
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2021-11-17 03:09:52 +0000

    Add expected behaviours for init subcommand tests

---
+++ tests/ai_market_contest/cli/test_initsubcommand.py
@@ -0,0 +1,28 @@
+﻿"""
+# Behaviour:
+- Should create an aicontest folder at the given location
+- Should take a -n flag specifying the number of agents to create at launch
+- The -n flag should default to 1 agent
+- The --include-example argument should include an example of running the
+  program
+"""
+
+from unittest import TestCase
+
+from cli import initialise_parser
+
+
+class InitSubcommandTest(TestCase):
+    parser = initialise_parser()
+
+    def create_aicontest_folder_at_given_path(self):
+        raise NotImplementedError
+
+    def specify_number_of_agents_to_initialise_with_using_n_flag(self):
+        raise NotImplementedError
+
+    def default_number_of_agents_to_create_is_one(self):
+        raise NotImplementedError
+
+    def copy_example_usage_with_include_example_tag(self):
+        raise NotImplementedError
