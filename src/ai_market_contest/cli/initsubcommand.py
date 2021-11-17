9877e99ff6079e8bac64d3ac47a4ca42f2c2106c
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-11-17 02:16:57 +0000

    Add a seperate folder for each agent

--- src/ai_market_contest/cli/initsubcommand.py
+++ src/ai_market_contest/cli/initsubcommand.py
@@ -5,7 +5,7 @@ import shutil
 import sys
 from typing import Any, List
 
-from config import (
+from cli_config import (
     AGENT_FILE,
     CONFIG_FILENAME,
     EXAMPLE_MAIN_FILE,
@@ -13,51 +13,17 @@ from config import (
     PROJ_DIR_NAME,
 )
 
-
-def make_agent_classname_camelcase(agent_name: str):
-    AGENT_STR = "agent"
-    if AGENT_STR.capitalize() in agent_name:
-        return agent_name
-    agent_name_cc = agent_name.lower()
-    if AGENT_STR in agent_name_cc:
-        agent_name_cc = agent_name_cc.replace(AGENT_STR, AGENT_STR.capitalize())
-    return agent_name_cc[0].upper() + agent_name_cc[1:]
+from utils import write_to_new_agent_file
 
 
 def make_agents_classes(proj_dir: pathlib.Path, agents_names: list[str]):
-    IMPORT_STR: str = "import"
-    AGENT_STR: str = "Agent"
-    ABS_METHOD_STR: str = "abstractmethod"
-    CLASS_METHOD_STR: str = "classmethod"
     for agent_name in agents_names:
         agent_filename: str = agent_name + ".py"
-        agent_file: pathlib.Path = proj_dir / agent_filename
+        agent_dir = proj_dir / agent_name
+        agent_dir.mkdir(parents=True)
+        agent_file: pathlib.Path = agent_dir / agent_filename
         agent_file.touch()
-        class_line_tab = False
-        with agent_file.open("w") as f1:
-            f1.write("from agent import Agent\n")
-            with AGENT_FILE.open("r") as f2:
-                for line in f2:
-                    if line is not None:
-                        if CLASS_METHOD_STR in line:
-                            break
-                        if IMPORT_STR in line:
-                            continue
-                        if ABS_METHOD_STR in line:
-                            continue
-                        if AGENT_STR in line:
-                            tab = "\t" if class_line_tab else ""
-                            f1.write(
-                                tab
-                                + "class "
-                                + make_agent_classname_camelcase(agent_name)
-                                + "(Agent):\n"
-                            )
-                            class_line_tab = True
-                        else:
-                            f1.write(line)
-            f2.close()
-        f1.close()
+        write_to_new_agent_file(agent_file, agent_name)
 
 
 def make_proj_dir(proj_dir: pathlib.Path):
--- src/ai_market_contest/cli/resetsubcommand.py
+++ src/ai_market_contest/cli/resetsubcommand.py
@@ -3,7 +3,7 @@ import shutil
 import sys
 from typing import Any
 
-from config import PROJ_DIR_NAME
+from cli_config import PROJ_DIR_NAME
 
 
 def remove_proj_dir(path_exists: bool, proj_dir: pathlib.Path):
