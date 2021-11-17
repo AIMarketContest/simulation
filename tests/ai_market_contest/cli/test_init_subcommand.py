2e9b23f97eae3d86cad79b05c1d122bf8adc48d6
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2021-11-17 04:03:16 +0000

    Add create aicontest folder test and fix structure

---
+++ tests/ai_market_contest/cli/test_init_subcommand.py
@@ -0,0 +1,44 @@
+﻿"""
+# Behaviour:
+- Should create an aicontest folder at the given location
+- Should take a -n flag specifying the number of agents to create at launch
+- The -n flag should default to 1 agent
+- The --include-example argument should include an example of running the
+  program
+"""
+
+import io
+import os
+import os.path
+import sys
+
+import pytest
+from cli import initialise_parser
+
+
+@pytest.fixture
+def parser():
+    return initialise_parser()
+
+
+def test_init_creates_aicontest_folder_at_given_path(tmp_path, parser):
+    # parse arguments
+    args = parser.parse_args(["init", str(tmp_path)])
+
+    # input for the program once it runs
+    sys.stdin = io.StringIO("AgentName\nAuthor Name\n")
+    args.func(args)
+
+    assert os.path.isdir(tmp_path / "aicontest")
+
+
+def test_can_specify_number_of_agents_to_initialise_with_using_n_flag():
+    raise NotImplementedError
+
+
+def test_default_number_of_agents_to_create_is_one():
+    raise NotImplementedError
+
+
+def test_will_copy_example_usage_with_include_example_tag():
+    raise NotImplementedError
