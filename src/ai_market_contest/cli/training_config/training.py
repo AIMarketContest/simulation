5fe5d15029517b2cb1c100940307fed952d0a0fc
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2021-11-19 12:31:10 +0000

    Add rudimentary training algorithm

---
+++ src/ai_market_contest/cli/training_config/training.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from ai_market_contest.agent import Agent
+from ai_market_contest.environment import Environment
+
+
+def agent_dict_to_list(agent_dict: dict[Agent, int], env: Environment) -> List[int]:
+    agent_values: List[int] = []
+    for agent_name in env.possible_agents:
+        agent_values.append(agent_dict[agent_name])
+    return agent_values
+
+
+def train(env):
+    agent_sales = {agent_name: 0 for agent_name in env.possible_agents}
+    previous_prices = {agent: 0 for agent in env.possible_agents}
+    current_prices = {agent: 0 for agent in env.possible_agents}
+
+    dones = {agent: False for agent in env.possible_agents}
+    while all(dones.values()):
+        print(previous_prices)
+        _, rewards, dones, _ = env.step(previous_prices)
+        for agent_name, reward in rewards.items():
+            agent_sales[agent_name] += reward
+
+        previous_prices_list = agent_dict_to_list(previous_prices, env)
+
+        for index, agent_name in enumerate(env.possible_agents):
+            current_prices[agent_name] = env.agent_name_mapping[agent_name].policy(
+                previous_prices_list, index
+            )
+
+        current_prices_list = agent_dict_to_list(current_prices, env)
+        for index, agent_name in enumerate(env.possible_agents):
+            env.agent_name_mapping[agent_name].update(
+                previous_prices_list,
+                rewards[agent_name],
+                current_prices_list,
+                index,
+            )
+
+        previous_prices = current_prices
