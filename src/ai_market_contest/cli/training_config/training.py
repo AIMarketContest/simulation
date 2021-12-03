85f38ecd3f64d9abb77ac50beafc6b87ca16ffd2
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-12-03 10:46:02 +0000

    Implement train method

--- src/ai_market_contest/cli/training_config/training.py
+++ src/ai_market_contest/cli/training_config/training.py
@@ -2,6 +2,13 @@ from typing import List
 
 from ai_market_contest.environment import Market
 
+import shutil
+
+import ray
+from ray.rllib import agents  # type: ignore
+from ray.tune.logger import pretty_print  # type: ignore
+from ray.tune.registry import register_env  # type: ignore
+
 
 def agent_dict_to_list(agent_dict: dict[str, int], env: Market) -> List[int]:
     agent_values: List[int] = []
@@ -14,31 +21,27 @@ def agent_dict_to_list(agent_dict: dict[str, int], env: Market) -> List[int]:
 
 
 def train(env):
-    agent_sales = {agent_name: 0 for agent_name in env.possible_agents}
-    previous_prices = {agent: 0 for agent in env.possible_agents}
-    current_prices = {agent: 0 for agent in env.possible_agents}
+    ray.init(ignore_reinit_error=True)
 
-    dones = {agent: False for agent in env.possible_agents}
-    while all(dones.values()):
-        print(previous_prices)
-        _, rewards, dones, _ = env.step(previous_prices)
-        for agent_name, reward in rewards.items():
-            agent_sales[agent_name] += reward
+    register_env(
+        "marketplace",
+        lambda x: env,
+    )
 
-        previous_prices_list = agent_dict_to_list(previous_prices, env)
+    config = agents.dqn.DEFAULT_CONFIG.copy()
+    config.update(
+        {
+            "num_workers": 1,
+        }
+    )
 
-        for index, agent_name in enumerate(env.possible_agents):
-            current_prices[agent_name] = env.agent_name_mapping[agent_name].policy(
-                previous_prices_list, index
-            )
+    trainer = agents.dqn.DQNTrainer(env="marketplace")
 
-        current_prices_list = agent_dict_to_list(current_prices, env)
-        for index, agent_name in enumerate(env.possible_agents):
-            env.agent_name_mapping[agent_name].update(
-                previous_prices_list,
-                rewards[agent_name],
-                current_prices_list,
-                index,
-            )
+    chkpt_root = "tmp/exa"
+    shutil.rmtree(chkpt_root, ignore_errors=True, onerror=None)
 
-        previous_prices = current_prices
+    for _ in range(800):
+        result = trainer.train()
+        print(pretty_print(result))
+
+    trainer.save(chkpt_root)
