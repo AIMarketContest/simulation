9f995b61cc98f0f44e48d23bf4a569aceca3122f
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2021-12-03 12:31:12 +0000

    Add code to run contest for policies

--- src/ai_market_contest/cli/utils/run_contest.py
+++ src/ai_market_contest/cli/utils/run_contest.py
@@ -3,30 +3,34 @@ from ai_market_contest.agent import Agent
 
 from ai_market_contest.environment import Market
 from ray.rllib.policy.policy import Policy
+from ray.tune.registry import register_env
 
 
 def run_contest(env: Market, agents: dict[Policy, str]):
+
     agent_sales: dict[Policy, list[float]] = {
-        agent_name: [] for agent_name in agents.keys
+        agent_name: [] for agent_name in agents.keys()
     }
     previous_actions: dict[str, float] = {}
     current_actions: dict[str, float] = {}
 
-    dones: dict[Policy, bool] = {agent: False for agent in agents.keys}
+    dones: dict[Policy, bool] = {agent: False for agent in agents.keys()}
     obs = env.reset()
-    for agent, agent_id in agents:
-        previous_actions[agent_id] = agent.compute_action(obs)
-    obs, rewards, dones, infos = env.step(current_actions)
+    for agent, agent_id in agents.items():
+        previous_actions[agent_id], _ = agent.compute_actions(obs)[0]
+    obs, rewards, dones, infos = env.step(previous_actions)
 
     while not all(dones.values()):
-        for agent, agent_id in agents:
-            current_actions[agent_id] = agent.compute_action(
+        for agent, agent_id in agents.items():
+            current_actions[agent_id], _ = agent.compute_actions(
                 obs,
-                previous_actions=previous_actions[agent_id],
-                prev_reward=rewards[agent],
-                info=infos,
-            )
+                prev_action_batch=previous_actions[agent_id],
+                prev_reward_batch=rewards[agent_id],
+                info_batch=infos,
+            )[0]
             agent_sales[agent].append(rewards[agent_id])
 
         previous_actions = current_actions
         obs, rewards, dones, infos = env.step(previous_actions)
+
+    return agent_sales
