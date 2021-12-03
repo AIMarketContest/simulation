ccdfd56e35b30dde56949348ade68f57e3997ae3
Michael Clasby <michaelclasby@hotmail.com>
2021-12-03 04:11:26 +0000

    Add config for training

--- src/rllib.py
+++ src/rllib.py
@@ -1,3 +1,7 @@
+import os
+import tempfile
+from datetime import datetime
+
 from ray.rllib import agents  # type: ignore
 from ray.tune.logger import pretty_print  # type: ignore
 from ray.tune.registry import register_env  # type: ignore
@@ -10,7 +14,32 @@ register_env(
     lambda x: Market(10, FixedDemandFunction(), 100),
 )
 
-trainer = agents.dqn.DQNTrainer(env="marketplace")
+config = agents.dqn.DEFAULT_CONFIG.copy()
+config.update(
+    {
+        "num_workers": 1,
+    }
+)
+
+
+def custom_log_creator(custom_path, custom_str):
+
+    timestr = datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
+    logdir_prefix = "{}_{}".format(custom_str, timestr)
+
+    def logger_creator(config):
+
+        if not os.path.exists(custom_path):
+            os.makedirs(custom_path)
+        logdir = tempfile.mkdtemp(prefix=logdir_prefix, dir=custom_path)
+        return UnifiedLogger(config, logdir, loggers=None)
+
+    return logger_creator
+
+
+trainer = agents.dqn.DQNTrainer(
+    env="marketplace", logger_creator=custom_log_creator("./logs", "marketplace")
+)
 
 
 for i in range(1000):
