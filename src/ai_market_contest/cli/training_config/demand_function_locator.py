4da0a796710c2e5889452f5aa25694d97219cbae
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-01-03 06:35:04 +0000

    Write skeleton for demand function locator

---
+++ src/ai_market_contest/cli/training_config/demand_function_locator.py
@@ -0,0 +1,10 @@
+import pathlib
+from ai_market_contest.demand_function import DemandFunction
+
+
+class DemandFunctionLocator:
+    def __init__(self, demand_function_path: pathlib.Path):
+        self.demand_function_path: pathlib.Path = demand_function_path
+
+    def get_demand_function(demand_function: str) -> DemandFunction:
+        raise NotImplementedError()
