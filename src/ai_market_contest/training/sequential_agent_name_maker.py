7e839ee3235aa024311d1910e5fe33e242ed8d11
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-01-03 03:42:08 +0000

    Define sequential agent name maker

---
+++ src/ai_market_contest/training/sequential_agent_name_maker.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from ai_market_contest.training.agent_name_maker import AgentNameMaker  # type: ignore
+
+
+class SequentialAgentNameMaker(AgentNameMaker):
+    MIN_NUM_AGENTS = 1
+
+    def __init__(self, number_of_agents):
+        if number_of_agents < self.MIN_NUM_AGENTS:
+            raise ValueError(f"Cannot have less than {self.MIN_NUM_AGENTS} agents")
+
+        self.number_of_agents = number_of_agents
+
+    def get_names(self) -> List[str]:
+        return [f"player_{i}" for i in range(self.number_of_agents)]
+
+    def get_name(self, num: int) -> str:
+        if num < 0 or num >= self.number_of_agents:
+            raise ValueError(
+                f"Agent index {num} is out of range, should be between 0 and {self.number_of_agents - 1}"
+            )
+        return f"player_{num}"
