fa0082cf156d81aad8d9a4893cd6a24e156b2fbc
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-01-05 21:26:45 +0000

    Fix typing in training config reader

--- src/ai_market_contest/cli/training_config/config_reader.py
+++ src/ai_market_contest/cli/training_config/config_reader.py
@@ -1,6 +1,7 @@
 import pathlib
+from ast import literal_eval
 from configparser import ConfigParser
-from typing import Dict
+from typing import Any, Dict
 
 from ai_market_contest.cli.utils.demand_function_locator import DemandFunctionLocator
 from ai_market_contest.environment import Market
@@ -20,16 +21,23 @@ class TrainingConfigReader:
         self.parsed_config: ConfigParser = config_parser
         self.demand_function_locator: DemandFunctionLocator = demand_function_locator
 
-    def get_naive_agent_nums(self) -> Dict[str, str]:
-        return self.parsed_config["Naive Agents"]
+    def get_self_play_num(self) -> int:
+        return int(self.parsed_config["General"]["number_of_self_play_agents"])
+
+    def get_naive_agent_counts(self) -> Dict[str, int]:
+        return {
+            (agent_name, int(agent_count))
+            for agent_name, agent_count in self.parsed_config["Naive Agents"]
+        }
 
     def get_num_agents(self) -> int:
-        return int(self.parsed_config["General"]["number_of_self_play_agents"]) + int(
-            len(self.parsed_config["Naive Agents"].keys())
-        )
+        return self.get_self_play_num + sum(self.get_naive_agent_counts().items())
 
-    def get_other_config(self) -> Dict[str, str]:
-        return self.parsed_config["Other"]
+    def get_other_config(self) -> Dict[str, Any]:
+        return {
+            (setting_name, literal_eval(setting_value))
+            for setting_name, setting_value in self.parsed_config["Other"]
+        }
 
     def get_environment(self, agent_name_maker: AgentNameMaker) -> Market:
         return Market(
