910ba9d87b0e47b2fe12f3ffae67169a1c95793f
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-01-05 18:15:48 +0000

    Make training extension a constant

--- src/ai_market_contest/cli/utils/config_utils.py
+++ src/ai_market_contest/cli/utils/config_utils.py
@@ -2,7 +2,10 @@ import sys
 from pathlib import Path
 from typing import List
 
-from ai_market_contest.cli.cli_config import TRAINING_CONFIGS_DIR_NAME
+from ai_market_contest.cli.cli_config import (
+    TRAINING_CONFIG_FILE_EXTENSION,
+    TRAINING_CONFIGS_DIR_NAME,
+)
 
 
 def check_configs(training_configs: List[str]) -> None:
@@ -16,6 +19,8 @@ def check_configs(training_configs: List[str]) -> None:
 def get_training_configs(proj_dir: Path):
     training_configs_dir = proj_dir / TRAINING_CONFIGS_DIR_NAME
     training_configs = []
-    for training_config_file in training_configs_dir.rglob("*.ini"):
+    for training_config_file in training_configs_dir.rglob(
+        f"*{TRAINING_CONFIG_FILE_EXTENSION}"
+    ):
         training_configs.append(training_config_file.stem)
     return training_configs
