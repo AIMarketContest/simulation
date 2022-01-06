5cb63045d858a4f1a408b890f968961e9153f17a
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-01-06 03:33:48 +0000

    Implement TrainingConfigMaker

---
+++ src/ai_market_contest/training/training_config_maker.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict
+
+from ai_market_contest.cli.training_config.config_reader import TrainingConfigReader
+from ai_market_contest.training.policy_config_maker import PolicyConfigMaker
+
+
+class TrainingConfigMaker:
+    def __init__(
+        self,
+        trainer_config_reader: TrainingConfigReader,
+        policy_config_maker: PolicyConfigMaker,
+    ):
+        self.training_config_reader: TrainingConfigReader = trainer_config_reader
+        self.policy_config_maker: PolicyConfigMaker = policy_config_maker
+
+    def make_training_config(self) -> Dict[str, Any]:
+        config: Dict[str, Any] = self.policy_config_maker.get_policy_config()
+        config.update(self.training_config_reader.get_other_config())
+        return config
