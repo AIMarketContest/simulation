b55c749c8b3f38ebe33df965c2161cc4ccf595ec
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-01-06 03:33:24 +0000

    Add types to PolicyConfigMaker

--- src/ai_market_contest/training/policy_config_maker.py
+++ src/ai_market_contest/training/policy_config_maker.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 
 from ray.rllib.policy.policy import PolicySpec
 
@@ -8,25 +8,26 @@ from ai_market_contest.training.policy_selector import PolicySelector
 
 class PolicyConfigMaker:
     def __init__(self, agent_locator: AgentLocator, policy_selector: PolicySelector):
-        self.policy_selector = policy_selector
-        self.agent_locator = agent_locator
+        self.policy_selector: PolicySelector = policy_selector
+        self.agent_locator: AgentLocator = agent_locator
 
-    def get_policy_config(self):
-        agent_to_train = self.policy_selector.get_agent_name()
-        policy_config = {"multiagent": {}}
-        policy_config["multiagent"]["policies_to_train"] = [agent_to_train_str]
+    def get_policy_config(self) -> Dict[str, Dict[str, Any]]:
+        agent_to_train: str = self.policy_selector.get_agent_name()
+        policy_config: Dict[str, Dict[str, Any]] = {"multiagent": {}}
+        policy_config["multiagent"]["policies_to_train"] = [agent_to_train]
         policy_config["multiagent"]["policies"] = {}
         policy_config["multiagent"]["policies"][agent_to_train] = PolicySpec(
-            policy_class=agentlocator.get_agent(agent_to_train)
+            policy_class=self.agentlocator.get_agent(agent_to_train)
         )
         if self.policy_selector.has_self_play():
-            agent_opponent = self.policy_selector.get_agent_opponent_name()
+            agent_opponent: str = self.policy_selector.get_agent_opponent_name()
             policy_config["multiagent"]["policies"][agent_opponent] = PolicySpec(
-                policy_class=agentlocator.get_agent(agent_opponent)
+                policy_class=self.agentlocator.get_agent(agent_opponent)
             )
+        naive_agent: str
         for naive_agent in self.policy_selector.get_naive_agents_names():
             policy_config["multiagent"]["policies"][naive_agent] = PolicySpec(
-                policy_class=agentlocator.get_agent(naive_agent)
+                policy_class=self.agentlocator.get_agent(naive_agent)
             )
         policy_config["multiagent"][
             "policy_mapping_fn"
