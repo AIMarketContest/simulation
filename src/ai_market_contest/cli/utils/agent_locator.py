25843e348ba66b9219ca0a24205b64d134b02f42
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-01-06 03:52:44 +0000

    Fix typing and most bugs in agent locator

--- src/ai_market_contest/cli/utils/agent_locator.py
+++ src/ai_market_contest/cli/utils/agent_locator.py
@@ -11,24 +11,24 @@ from ai_market_contest.demand_function import DemandFunction
 
 class AgentLocator:
     def __init__(self, agents_dir: pathlib.Path):
-        self.agents_dir = agents_dir
+        self.agents_dir: pathlib.Path = agents_dir
 
     def _get_agents(self) -> List[str]:
         config_parser: ConfigParser = ConfigParser()
         config_parser.read(self.agents_dir / CONFIG_FILENAME)
         agents: List[str] = literal_eval(
-            config_parser["agentsironment"]["demand functions"]
+            config_parser["agentsenvironment"]["demand functions"]
         )
         return agents
 
-    def get_agent(self, agent_name: str) -> DemandFunction:
+    def get_agent(self, agent_name: str) -> Agent:
         if agent_name in CUR_AGENTS:
             return CUR_AGENTS[agent_name]
         agent_dir: pathlib.Path = self.agents_dir / agent_name
-        agent_file: str = agent_dir / agent_name + ".py"
+        agent_file: str = agent_dir / (agent_name + ".py")
         spec = importlib.util.spec_from_file_location(agent_name, agent_file)
         if spec.loader is None:
             raise Exception("Error in finding the required agent")
-        spec.loader.exec_module(demand_func_module)  # type: ignore
-        agent_cls = getattr(demand_func_module, agent_name)  # type: ignore
+        spec.loader.exec_module(agent_module)  # type: ignore
+        agent_cls = getattr(agent_module, agent_name)  # type: ignore
         return agent_cls
