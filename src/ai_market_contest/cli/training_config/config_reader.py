55c31f77d8b08bd2ef6a169874ea4640d372e3fa
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-01-06 01:18:39 +0000

    Fix errors with dictionaries in config_reader

--- src/ai_market_contest/cli/training_config/config_reader.py
+++ src/ai_market_contest/cli/training_config/config_reader.py
@@ -25,18 +25,20 @@ class TrainingConfigReader:
         return int(self.parsed_config["General"]["number_of_self_play_agents"])
 
     def get_naive_agent_counts(self) -> Dict[str, int]:
+        agent_name: str
+        agent_count: str
         return {
-            (agent_name, int(agent_count))
-            for agent_name, agent_count in self.parsed_config["Naive Agents"]
+            agent_name: int(agent_count)
+            for agent_name, agent_count in self.parsed_config["Naive Agents"].items()
         }
 
     def get_num_agents(self) -> int:
-        return self.get_self_play_num + sum(self.get_naive_agent_counts().items())
+        return self.get_self_play_num() + sum(self.get_naive_agent_counts().values())
 
     def get_other_config(self) -> Dict[str, Any]:
         return {
-            (setting_name, literal_eval(setting_value))
-            for setting_name, setting_value in self.parsed_config["Other"]
+            setting_name: literal_eval(setting_value)
+            for setting_name, setting_value in self.parsed_config["Other"].items()
         }
 
     def get_environment(self, agent_name_maker: AgentNameMaker) -> Market:
