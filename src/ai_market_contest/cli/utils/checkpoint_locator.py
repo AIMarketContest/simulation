f8b0803deb80421069c45c77e9870fae476d7c58
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2022-01-07 12:12:19 +0000

    Add a existing dir check in checkpoint locator

--- src/ai_market_contest/cli/utils/checkpoint_locator.py
+++ src/ai_market_contest/cli/utils/checkpoint_locator.py
@@ -1,5 +1,11 @@
 import pathlib
+from ai_market_contest.cli.utils.filesystemutils import check_directory_exists
 
 
-def get_checkpoint_path(dir_path: pathlib.Path):
-    return str((dir_path / "checkpoint_000001/checkpoint-1").resolve())
+def get_checkpoint_path(dir_path: pathlib.Path) -> str:
+    checkpoint_path: pathlib.Path = (
+        dir_path / "checkpoint_000001/checkpoint-1"
+    ).resolve()
+    error_msg: str = "No checkpoint exists at " + str(checkpoint_path)
+    check_directory_exists(checkpoint_path, error_msg)
+    return str(checkpoint_path)
