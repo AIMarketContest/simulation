9231ae12e695b97d6c772ad6a2d7d2369ceb9f6b
Jack Benham <jack.benham34@gmail.com>
2022-01-07 00:42:32 +0000

    Updated noisy_fixed_demand_function.py to no longer take constructor parameters

--- src/ai_market_contest/demandfunctions/noisy_fixed_demand_function.py
+++ src/ai_market_contest/demandfunctions/noisy_fixed_demand_function.py
@@ -3,16 +3,15 @@ from numpy.random import randint
 from ai_market_contest.demand_function import DemandFunction
 from typing import Dict
 
-
 class NoisyFixedDemandFunction(DemandFunction):
-    def __init__(self, fixed_quantity: int = 1000):
-        if fixed_quantity < 0:
-            raise ValueError("fixed_quantity must be greater than or equal to 0")
+    FIXED_QUANTITY: int = 1000
 
-        self.fixed_quantity: int = fixed_quantity
+    def __init__(self):
+        if self.FIXED_QUANTITY < 0:
+            raise ValueError("fixed_quantity must be greater than or equal to 0")
 
     def get_sales(self, current_prices: Dict[str, int]) -> Dict[str, int]:
         return {
-            agent: int(randint(0, 100) + self.fixed_quantity)
+            agent: int(randint(0, 100) + self.FIXED_QUANTITY)
             for agent in current_prices.keys()
         }
