e9f16603643a320fd8fd4c7efd1d2067abb4d402
Jack Benham <jack.benham34@gmail.com>
2022-01-07 23:37:59 +0000

    Fixed Basic Agent Tests

--- tests/ai_market_contest/agents/test_basic_agents.py
+++ tests/ai_market_contest/agents/test_basic_agents.py
@@ -1,19 +1,32 @@
-from ai_market_contest.agents.fixed_agent import FixedAgent
+from ai_market_contest.agents.fixed_agent_fifty import FixedAgentFifty
+from ai_market_contest.agents.fixed_agent_random import FixedAgentRandom
 from ai_market_contest.agents.random_agent import RandomAgent
 
 rand_agent = RandomAgent()
-fixed_agent = FixedAgent()
+fixed_agent_fifty = FixedAgentFifty()
+fixed_agent_rand = FixedAgentRandom()
 
-agents = [rand_agent, fixed_agent]
+
+def test_fixed_price_agent_fifty_gives_returns_correct_price():
+    first_price = fixed_agent_fifty.policy([], 0)
+    second_price = fixed_agent_fifty.policy([], 0)
+
+    fixed_agent_fifty.update([], 0)
+
+    third_price = fixed_agent_fifty.policy([], 0)
+
+    assert first_price == 50
+    assert first_price == second_price
+    assert first_price == third_price
 
 
-def test_fixed_price_agent_gives_returns_fixed_price():
-    first_price = fixed_agent.policy([], 0)
-    second_price = fixed_agent.policy([], 0)
+def test_fixed_random_agent_generates_consistent_prices() :
+    first_price = fixed_agent_fifty.policy([], 0)
+    second_price = fixed_agent_fifty.policy([], 0)
 
-    fixed_agent.update([0], 10, [0], 10, 4)
+    fixed_agent_fifty.update([], 0)
 
-    third_price = fixed_agent.policy([], 0)
+    third_price = fixed_agent_fifty.policy([], 0)
 
     assert first_price == second_price
     assert first_price == third_price
@@ -23,7 +36,7 @@ def test_random_agent_uses_generates_different_prices():
     first_price = rand_agent.policy([], 0)
     second_price = rand_agent.policy([], 0)
 
-    rand_agent.update([0], 10, [0], 10, 0)
+    rand_agent.update([], 0)
 
     third_price = rand_agent.policy([], 0)
 
