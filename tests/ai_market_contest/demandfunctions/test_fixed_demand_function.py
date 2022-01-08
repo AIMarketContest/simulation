eb5ce23d85e56467de020b557f5590fa4d52da83
Jack Benham <jack.benham34@gmail.com>
2022-01-08 18:36:43 +0000

    Implemented Fixed Demand Function tests

--- tests/ai_market_contest/demandfunctions/test_fixed_demand_function.py
+++ tests/ai_market_contest/demandfunctions/test_fixed_demand_function.py
@@ -5,30 +5,22 @@ from ai_market_contest.demandfunctions.fixed_demand_function import FixedDemandF
 
 
 class FixedDemandFunctionTest(TestCase):
-    demand_function_with_quantity_of_four: DemandFunction = FixedDemandFunction(4)
-    demand_function_with_quantity_of_six: DemandFunction = FixedDemandFunction(6)
+    demand_function: DemandFunction = FixedDemandFunction()
 
     def test_function_returns_correct_number_of_demands(self):
-        sales_with_four_prices: list[
-            int
-        ] = self.demand_function_with_quantity_of_four.get_sales([0.4, 0.3, 0.5, 0.5])
+        sales_with_four_prices = self.demand_function.get_sales({
+            "agent_0": 10,
+            "agent_1": 15,
+            "agent_2": 10,
+            "agent_3": 8
+        })
         assert len(sales_with_four_prices) == 4
-        sales_with_three_prices: list[
-            int
-        ] = self.demand_function_with_quantity_of_four.get_sales([0.7, 0.3, 0.5])
+        sales_with_three_prices = self.demand_function.get_sales({
+            "agent_0": 10,
+            "agent_1": 15,
+            "agent_2": 10
+        })
         assert len(sales_with_three_prices) == 3
 
-    def test_function_returns_fixed_quantity(self):
-        assert self.demand_function_with_quantity_of_four.get_sales([0.5])[0] == 4
-
-        assert self.demand_function_with_quantity_of_six.get_sales([0.3])[0] == 6
-
-    def test_function_returns_list_with_only_same_element(self):
-        assert (
-            len(set(self.demand_function_with_quantity_of_four.get_sales([0.1, 0.2])))
-            == 1
-        )
-        assert (
-            len(set(self.demand_function_with_quantity_of_six.get_sales([0.1, 0.2])))
-            == 1
-        )
+    def test_function_returns_single_sale(self):
+        assert self.demand_function.get_sales({"agent": 10})["agent"] == 1
