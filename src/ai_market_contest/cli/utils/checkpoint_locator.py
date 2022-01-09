7e4f03fff1e572e080f1ca4ea2ee05c76cd37368
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-01-09 05:33:10 +0000

    Fix strings in checkpoint locator

--- src/ai_market_contest/cli/utils/checkpoint_locator.py
+++ src/ai_market_contest/cli/utils/checkpoint_locator.py
@@ -10,14 +10,16 @@ def get_checkpoint_path(
     checkpoint_should_exist: bool,
     training_config_reader: TrainingConfigReader,
 ) -> str:
-    epochs_str = str(training_config_reader.get_num_epochs())
+    NUM_CHECKPOINT_DIGITS: Final = 6
+    epochs_str: str = str(training_config_reader.get_num_epochs())
     checkpoint_path: pathlib.Path = (
-        dir_path / "checkpoint_"
-        + "0" * (6 - len(epochs_str))
-        + epochs_str / "checkpoint-"
-        + str(epochs)
+        dir_path
+        / (
+            f"checkpoint_{'0'*(NUM_CHECKPOINT_DIGITS - len(epochs_str))}"
+            + f"{epochs_str}/checkpoint-{epochs_str}"
+        )
     ).resolve()
     if checkpoint_should_exist:
-        error_msg: str = "No checkpoint exists at " + str(checkpoint_path)
+        error_msg: str = f"No checkpoint exists at {str(checkpoint_path)}"
         check_file_exists(checkpoint_path, error_msg)
     return str(checkpoint_path)
