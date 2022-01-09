03af077c5dc79b0e412b22d57d8ddf32581ce46c
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2022-01-09 02:13:29 +0000

    Fix bug with naive agents in evaluator

--- src/ai_market_contest/evaluation/agent_evaluator.py
+++ src/ai_market_contest/evaluation/agent_evaluator.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, Tuple  # type: ignore
 import gym  # type: ignore
 from ray.rllib.agents.registry import get_trainer_class  # type: ignore
 from ray.rllib.agents.trainer import Trainer  # type: ignore
+from ray.tune.registry import register_env  # type: ignore
 
 from ai_market_contest.agent import Agent  # type: ignore
 from ai_market_contest.cli.utils.agent_locator import AgentLocator  # type: ignore
@@ -25,6 +26,7 @@ class AgentEvaluator:
         op_algorithm: str,
         agent_name_maker: AgentNameMaker,
     ):
+        register_env("marketplace", lambda x: env)
         self.env = env
         self.naive_agents_map: Dict[str, Tuple[Agent, int]] = {}
         index: int = 0
@@ -43,16 +45,15 @@ class AgentEvaluator:
         self.trainers = {}
         for agent_name, chosen_agent_version in agents.items():
             trainer_cls: Trainer = get_trainer_class(op_algorithm)
-            new_trainer: Trainer = trainer_cls()
+            new_trainer: Trainer = trainer_cls(
+                env="marketplace", config={"num_workers": 1, explore: False}
+            )
             checkpoint_path: str = get_checkpoint_path(chosen_agent_version.get_dir())
             new_trainer.restore(checkpoint_path)
             self.trainers[agent_name] = new_trainer
             self.agent_name_map[agent_name] = agent_name_maker.get_name(index)
+            self.reversed_agent_name_map[agent_name_maker.get_name(index)] = agent_name
             index += 1
-        self.reversed_agent_name_map = {
-            env_agent_name: agent_name
-            for agent_name, env_agent_name in self.agent_name_map.items()
-        }
 
     def evaluate(self) -> None:
         done = False
@@ -64,13 +65,13 @@ class AgentEvaluator:
             observed_actions = {}
             for naive_agent_str, naive_agent in self.naive_agents_map.items():
                 env_agent_name = self.agent_name_map[naive_agent_str]
-                action = naive_agent.policy(obs[naive_agent_str], 0)
+                action = naive_agent.policy(obs[env_agent_name], 0)
                 actions[naive_agent_str] = action
                 observed_actions[env_agent_name] = action
 
             for agent_name, trainer in self.trainers.items():
                 env_agent_name = self.agent_name_map[agent_name]
-                action = trainer.compute_action(obs[env_agent_name])
+                action = trainer.compute_action(obs)
                 actions[agent_name] = action
                 observed_actions[env_agent_name] = action
 
