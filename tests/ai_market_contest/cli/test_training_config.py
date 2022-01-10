b148bdb98fe64d138bc80b57e865acd8442e4cd6
Jack Benham <jack.benham34@gmail.com>
2022-01-10 21:51:28 +0000

    Add tests for training configs

---
+++ tests/ai_market_contest/cli/test_training_config.py
@@ -0,0 +1,34 @@
+from configparser import ConfigParser
+
+from importlib_metadata import pathlib
+from src.ai_market_contest.training.training_config_maker import TrainingConfigMaker
+from ai_market_contest.cli.utils.project_initialisation_utils import initialise_file_structure
+from ai_market_contest.cli.configs.training_config_reader import TrainingConfigReader
+from ai_market_contest.training.policy_config_maker import PolicyConfigMaker
+from ai_market_contest.cli.configs.simulation_config_reader import SimulationConfigReader
+from ai_market_contest.cli.utils.demand_function_locator import DemandFunctionLocator
+from ai_market_contest.training.sequential_agent_name_maker import SequentialAgentNameMaker
+
+def test_training_config(tmp_path):
+    tmp_path = tmp_path / "aic"
+    initialise_file_structure(tmp_path, ["TestAgent"], ["TestAuthor"])
+
+    demand_function_locator = DemandFunctionLocator(tmp_path)
+    config_parser = ConfigParser()
+    config_parser.optionxform = str
+    training_config_reader = TrainingConfigReader(tmp_path/"training_configs/training_example_config.ini", demand_function_locator, config_parser)
+
+    assert training_config_reader.get_naive_agent_counts() == {'FixedFifty': 3, 'Random': 1}
+    assert training_config_reader.get_other_config() == {'num_workers': 4}
+    assert training_config_reader.get_num_epochs() == 20
+    assert training_config_reader.print_training()
+
+    agent_name_maker = SequentialAgentNameMaker(4)
+    market = training_config_reader.get_environment(agent_name_maker)
+
+    assert market.agents == ['player_0', 'player_1', 'player_2', 'player_3']
+    assert market.simulation_length == 200
+
+    training_config_reader.write_config_to_file(tmp_path/"training_configs")
+    assert (tmp_path/"training_configs/config.ini").exists()
+
