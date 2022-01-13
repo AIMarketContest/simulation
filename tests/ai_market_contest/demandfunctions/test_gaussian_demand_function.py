64909fc75feedbb5e6cab09793a9357ecd182651
Pranav Maganti <pranav.maganti@gmail.com>
2022-01-13 01:59:49 +0000

    Update gaussian demand tests

--- tests/ai_market_contest/demandfunctions/test_gaussian_demand_function.py
+++ tests/ai_market_contest/demandfunctions/test_gaussian_demand_function.py
@@ -11,26 +11,22 @@ class GaussianDemandFunctionTest(TestCase):
     demand_function_with_sf_ten: DemandFunction = GaussianDemandFunction()
 
     def test_function_returns_correct_number_of_demands(self):
-        sales_with_four_prices: list[
-            int
-        ] = self.demand_function_with_default_sf.get_sales(
+        sales_with_four_prices = self.demand_function_with_default_sf.get_sales(
             {"agent_0": 10, "agent_1": 15, "agent_2": 10, "agent_3": 8}
         )
         assert len(sales_with_four_prices) == 4
-        sales_with_three_prices: list[
-            int
-        ] = self.demand_function_with_default_sf.get_sales(
+        sales_with_three_prices = self.demand_function_with_default_sf.get_sales(
             {"agent_0": 10, "agent_1": 15, "agent_2": 10}
         )
         assert len(sales_with_three_prices) == 3
 
     def test_function_returns_gaussian_(self):
         assert (
-            self.demand_function_with_default_sf.get_sales({"agent_0": 0.5})["agent_0"]
-            == 308
+            self.demand_function_with_default_sf.get_sales({"agent_0": 50})["agent_0"]
+            == 100
         )
 
         assert (
-            self.demand_function_with_sf_ten.get_sales({"agent_0": 0.3})["agent_0"]
-            == 382
+            self.demand_function_with_sf_ten.get_sales({"agent_0": 30})["agent_0"]
+            == 100
         )
