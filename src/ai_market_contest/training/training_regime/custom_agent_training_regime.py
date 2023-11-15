e3b35d46ad35d0720727249daafec36b9d641a07
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-11-15 18:37:04 +0000

    Move epochs closer to use

--- src/ai_market_contest/training/training_regime/custom_agent_training_regime.py
+++ src/ai_market_contest/training/training_regime/custom_agent_training_regime.py
@@ -5,16 +5,9 @@ import typer
 from ray.rllib.agents.trainer import Trainer
 
 from ai_market_contest.agent import Agent
-from ai_market_contest.cli.cli_config import (
-    AGENTS_DIR_NAME,
-    DEFAULT_INITIAL_AGENT_PRICE,
-    ENVS_DIR_NAME,
-)
+from ai_market_contest.cli.cli_config import DEFAULT_INITIAL_AGENT_PRICE
 from ai_market_contest.cli.configs.agent_config_reader import AgentConfigReader
 from ai_market_contest.cli.configs.training_config_reader import TrainingConfigReader
-from ai_market_contest.cli.utils.agent_locator import AgentLocator
-from ai_market_contest.cli.utils.config_utils import get_training_config_path
-from ai_market_contest.cli.utils.demand_function_locator import DemandFunctionLocator
 from ai_market_contest.cli.utils.execute_training_routine import save_new_custom_agent
 from ai_market_contest.cli.utils.existing_agent.existing_agent_version import (
     ExistingAgentVersion,
@@ -44,12 +37,11 @@ class CustomAgentTrainingRegime(TrainingRegime):
 
     def execute(self) -> None:
         # Assumes agent to train is always first in the list
-        epochs = self.training_config_reader.get_epochs()
         agent_name_maker: AgentNameMaker = SequentialAgentNameMaker(
             self.training_config_reader.get_num_agents()
         )
-
         env = self.training_config_reader.get_environment(agent_name_maker)
+
         self_play_agents = self.training_config_reader.get_self_play_agents(
             self.agent_version
         )
@@ -62,6 +54,7 @@ class CustomAgentTrainingRegime(TrainingRegime):
         agents.extend(self_play_agents + naive_agents)
         agents.extend(trained_agents)
 
+        epochs = self.training_config_reader.get_epochs()
         cumulative_profits: list[int] = []
         for epoch in range(epochs):
             current_prices: dict[str, int] = {}
