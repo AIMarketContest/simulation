5c1ae4495ab06610f5785d1173eefa958f39d700
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-11-16 04:48:26 +0000

    Move get_agents error codes to class

--- src/ai_market_contest/cli/utils/error_codes.py
+++ src/ai_market_contest/cli/utils/error_codes.py
@@ -5,3 +5,5 @@ class ErrorCodes:
     DIRECTORY_DOES_NOT_EXIST: Final[int] = 2
     FILE_DOES_NOT_EXIST: Final[int] = 2
     NO_TRAINING_CONFIGS: Final[int] = 1
+    NO_AGENTS_ATTRIBUTE: Final[int] = 1
+    NO_TRAINED_AGENTS_ATTRIBUTE: Final[int] = 1
